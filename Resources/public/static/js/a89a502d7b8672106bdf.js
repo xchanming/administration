494558cc10d6b603e86022974f2587ad23403f9d
gxiaosong <lyricsong47@gmail.com>
2025-01-16 10:10:19 +0000

    chore: Change code annotations to after-sales

--- Resources/public/static/js/a89a502d7b8672106bdf.js
+++
@@ -1,6 +0,0 @@
-(window.webpackJsonpAdministration=window.webpackJsonpAdministration||[]).push([[12314],{899288:function(){},812314:function(t,i,e){"use strict";e.r(i),e.d(i,{default:function(){return o}}),e(87592);var o={template:'<div class="sw-cms-preview-html">\n    <sw-code-editor\n        v-model:value="demoValue"\n        :disabled="true"\n        :editor-config="editorConfig"\n    />\n</div>\n',compatConfig:Cicada.compatConfig,data(){return{demoValue:`
-<h2>Lorem ipsum dolor</h2>
-<p>Lorem ipsum dolor sit amet</p>
-<button type="button">
-    Click me!
-</button>`.trim(),editorConfig:{highlightActiveLine:!1,cursorStyle:"slim",highlightGutterLine:!1,showFoldWidgets:!1}}}}},87592:function(t,i,e){var o=e(899288);o.__esModule&&(o=o.default),"string"==typeof o&&(o=[[t.id,o,""]]),o.locals&&(t.exports=o.locals),e(745346).Z("6e4ee574",o,!0,{})}}]);
--- Resources/public/static/js/f52070fde7427bd6ad93.js
+++
@@ -1,1 +0,0 @@
-"use strict";(window.webpackJsonpAdministration=window.webpackJsonpAdministration||[]).push([[1818],{901818:function(n,o,i){i.r(o),i.d(o,{default:function(){return t}});var t={template:'\n{% block sw_cms_block_product_slider %}\n<div class="sw-cms-block-product-slider">\n    <slot name="productSlider"></slot>\n</div>\n{% endblock %}\n',compatConfig:Cicada.compatConfig}}}]);
